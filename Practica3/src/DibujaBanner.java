944b93bc0b39b00fb81b7fc13e67db600a32673a
nacho <peronabelmonte@gmail.com>
2018-11-11 19:00:45 +0100

    DIBIJO BANNER CON SOLO LA A

---
+++ Practica3/src/DibujaBanner.java
@@ -0,0 +1,30 @@
+
+public class DibujaBanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static String dibujaA (int numLinea) {
+	
+		switch(numLinea) {
+		case '0':
+			return "AAAAAAA ";
+		case 1:
+			return "A     A ";
+		case 2:
+			return "A     A ";
+		case 3:
+			return "AAAAAAA ";
+		case 4:
+			return "A     A ";
+		case 5: 
+			return "A     A ";
+		case 6:
+			return "A     A ";
+		}
+		return "error";
+	}
+
+}
